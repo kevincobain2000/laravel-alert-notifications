07b0068999cb8b2212b153708a5764649e490381
Pulkit Kathuria <kevincobain2000@gmail.com>
2019-03-31 14:15:11 +0900

    use guzzle instead of curl

--- src/Dispatcher/Webhook.php
+++ src/Dispatcher/Webhook.php
@@ -1,27 +1,17 @@
 <?php
 namespace Kevincobain2000\LaravelAlertNotifications\Dispatcher;
 
+use GuzzleHttp\Client;
+
 class Webhook
 {
     public static function send(string $url, array $body, $proxy, string $method = 'POST')
     {
-        $ch = curl_init($url);
-        $json = json_encode($body);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($json)
+        $client = new Client();
+        $result = $client->request($method, $url, [
+            'proxy' => $proxy,
+            'body' => json_encode($body)
         ]);
-
-        if ($proxy) {
-            curl_setopt($ch, CURLOPT_PROXY, $proxy);
-        }
-        $result = curl_exec($ch);
-        curl_close($ch);
         return $result;
     }
 }
