b334499115172ef670a68f523b00958cbf6ddbab
Miller, Andrew | Andrew | GATD <andrew.miller@rakuten.com>
2025-06-12 13:58:11 +0900

    Simplify AlertDispatchFailedException

--- src/Exceptions/AlertDispatchFailedException.php
+++ src/Exceptions/AlertDispatchFailedException.php
@@ -7,28 +7,20 @@ use Throwable;
 
 class AlertDispatchFailedException extends Exception
 {
+    public $exceptions = [];
+
     public function __construct($message = "Alert dispatch failed", $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
-    public static function mailFailed(Throwable $exception): self
+    public static function dispatchFailed(array $exceptions = []): self
     {
-        return new static("Mail dispatch failed with exception: {$exception->getMessage()}", 100, $exception);
-    }
-
-    public static function microsoftTeamsFailed(Throwable $exception): self
-    {
-        return new static("Microsoft Teams dispatch failed with exception: {$exception->getMessage()}", 200, $exception);
-    }
-
-    public static function slackFailed(Throwable $exception): self
-    {
-        return new static("Slack dispatch failed with exception: {$exception->getMessage()}", 300, $exception);
-    }
-
-    public static function pagerDutyFailed(Throwable $exception): self
-    {
-        return new static("Pager Duty dispatch failed with exception: {$exception->getMessage()}", 400, $exception);
+        $instance = new static("Alert dispatch failed", 0);
+        $instance->exceptions = $exceptions;
+        $instance->message = "Alert dispatch failed with the following exceptions:\n" . implode("\n", array_map(function ($e) {
+            return $e->getMessage();
+        }, $exceptions));
+        return $instance;
     }
 }
