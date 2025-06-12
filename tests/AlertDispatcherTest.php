36786ba80d4c3bcb35f1ef18d7aee136297d02de
Miller, Andrew | Andrew | GATD <andrew.miller@rakuten.com>
2025-06-12 13:58:59 +0900

    Add tests for failing dispatch methods

--- tests/AlertDispatcherTest.php
+++ tests/AlertDispatcherTest.php
@@ -4,9 +4,12 @@ namespace Kevincobain2000\LaravelEmailExceptions\Tests;
 
 use Exception;
 use Kevincobain2000\LaravelAlertNotifications\Dispatcher\AlertDispatcher;
+use Kevincobain2000\LaravelAlertNotifications\Exceptions\AlertDispatchFailedException;
+use Kevincobain2000\LaravelAlertNotifications\Exceptions\AlertDispatchMethodFailedException;
 use Mail;
 use Mockery;
 use Orchestra\Testbench\TestCase;
+use Throwable;
 
 class AlertDispatcherTest extends TestCase
 {
@@ -218,5 +221,257 @@ class AlertDispatcherTest extends TestCase
         $alertDispatcher = new AlertDispatcher(new Exception('Test Exception'));
 
         $this->assertTrue($alertDispatcher->shouldAlert());
+    }
+
+    public static function failedDispatchProvider()
+    {
+        $mailFailedException      = new Exception('Mail failed');
+        $teamsFailedException     = new Exception('Teams failed');
+        $slackFailedException     = new Exception('Slack failed');
+        $pagerDutyFailedException = new Exception('PagerDuty failed');
+
+        yield 'mail fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($mailFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'teams fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($teamsFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($teamsFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'slack fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($slackFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($slackFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'pager duty fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and teams fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $teamsFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($mailFailedException, $teamsFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and slack fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $slackFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($mailFailedException, $slackFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and pager duty fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($mailFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+                
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'teams and slack fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($teamsFailedException, $slackFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($teamsFailedException, $slackFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'teams and pager duty fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($teamsFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($teamsFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'slack and pager duty fail' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($slackFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($slackFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and teams and slack fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $teamsFailedException, $slackFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andReturn(null);
+            },
+            'expectedException' => function () use ($mailFailedException, $teamsFailedException, $slackFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and teams and pager duty fails' =>[
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $teamsFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($mailFailedException, $teamsFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'mail and slack and pager duty fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($mailFailedException, $slackFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andThrow($mailFailedException);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($mailFailedException, $slackFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('mail', $mailFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+
+        yield 'teams and slack and pager duty fails' => [
+            'expectedDispatchState' => function ($alertDispatcher) use ($teamsFailedException, $slackFailedException, $pagerDutyFailedException) {
+                $alertDispatcher->shouldReceive('sendMail')->andReturn(null);
+                $alertDispatcher->shouldReceive('sendMicrosoftTeams')->andThrow($teamsFailedException);
+                $alertDispatcher->shouldReceive('sendSlack')->andThrow($slackFailedException);
+                $alertDispatcher->shouldReceive('sendPagerDuty')->andThrow($pagerDutyFailedException);
+            },
+            'expectedException' => function () use ($teamsFailedException, $slackFailedException, $pagerDutyFailedException) {
+                $exceptions   = [];
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('microsoftTeams', $teamsFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('slack', $slackFailedException);
+                $exceptions[] = AlertDispatchMethodFailedException::methodFailed('pagerDuty', $pagerDutyFailedException);
+
+                return AlertDispatchFailedException::dispatchFailed($exceptions);
+            },
+        ];
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('failedDispatchProvider')]
+    public function testFailureInDispatchMethodDoesNotImpactOtherDispatchMethods(callable $expectedDispatchState, callable $expectedException)
+    {
+        Mail::fake();
+        config($this->config);
+
+        $alertDispatcher = Mockery::mock(
+            AlertDispatcher::class,
+            [new Exception('Test Exception')]
+        )->makePartial()->shouldAllowMockingProtectedMethods();
+
+        $expectedDispatchState($alertDispatcher);
+
+        try {
+            $alertDispatcher->notify();
+        } catch (Throwable $e) {
+            $this->assertEquals($expectedException(), $e);
+        }
     }
 }
